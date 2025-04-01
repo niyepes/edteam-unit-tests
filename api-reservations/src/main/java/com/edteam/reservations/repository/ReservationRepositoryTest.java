b9ec7a4419052d74836a78946e8a8c0385f728e5
Nicolás Yepes <nicolasyepes2004@gmail.com>
2025-04-01 11:50:46 -0500

    Se cambió el nombre del método my_first_test a getReservation_should_return_the_information para seguir el estándar de nombrado de los test , y en ese mismo sentido se creó el método  getReservation_should_not_return_the_information, que prueba exactamente lo contrario. También en su interior, se separaron con comentarios las secciones Given,When,Then; siguiendo la forma de estructurar los tests unitarios

--- api-reservations/src/main/java/com/edteam/reservations/repository/ReservationRepositoryTest.java
+++ api-reservations/src/main/java/com/edteam/reservations/repository/ReservationRepositoryTest.java
@@ -5,14 +5,31 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ReservationRepositoryTest {
 
     @Test
-    void my_first_test() {
+    void getReservation_should_return_the_information() {
+        //Given
         ReservationRepository repository = new ReservationRepository();
+
+        //WHen
         Optional<Reservation> result = repository.getReservationById(1L);
+
+        //Then
         assertEquals(1L, result.get().getId());
+    }
+
+    @Test
+    void getReservation_should_not_return_the_information() {
+        //Given
+        ReservationRepository repository = new ReservationRepository();
+
+        //WHen
+        Optional<Reservation> result = repository.getReservationById(1L);
+
+        //Then
+        assertTrue(result.isEmpty());
     }
 }
